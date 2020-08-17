9c12854ecd281c68664ff9cc75d424efe1d48f3f
frisch <frisch@cloudera.com>
2020-08-17 12:21:47 +0200

    deploy(cdp): add 7.1.3.0

--- src/main/resources/launchToPlatform.sh
+++ src/main/resources/launchToPlatform.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # Export Machine name to be  able to launch this script
-# export MACHINE=
+# export HOST=
 
 export USER=root
 export DIR=/home/root/yarn-submit
 
 # Send yarn-submit.jar & Config file & yarn-submit.sh
-ssh ${USER}@${MACHINE} "rm -rf ${DIR}/"
-ssh ${USER}@${MACHINE} "mkdir -p ${DIR}/"
-scp target/yarn-submit-*.jar ${USER}@${MACHINE}:${DIR}/yarn-submit.jar
-scp src/main/resources/parameters.properties ${USER}@${MACHINE}:${DIR}/
-scp src/main/resources/yarn-submit.sh ${USER}@${MACHINE}:${DIR}/
-scp src/main/resources/commandsToLaunch ${USER}@${MACHINE}:${DIR}/
-ssh ${USER}@${MACHINE} "chmod +x ${DIR}/yarn-submit.sh"
+ssh ${USER}@${HOST} "rm -rf ${DIR}/"
+ssh ${USER}@${HOST} "mkdir -p ${DIR}/"
+scp target/yarn-submit-*.jar ${USER}@${HOST}:${DIR}/yarn-submit.jar
+scp src/main/resources/parameters.properties ${USER}@${HOST}:${DIR}/
+scp src/main/resources/yarn-submit.sh ${USER}@${HOST}:${DIR}/
+scp src/main/resources/commandsToLaunch ${USER}@${HOST}:${DIR}/
+ssh ${USER}@${HOST} "chmod +x ${DIR}/yarn-submit.sh"
 
 # SSH on the machine and run or run it directly from here:
-ssh ${USER}@${MACHINE} "cd ${DIR} ;
+ssh ${USER}@${HOST} "cd ${DIR} ;
 ./yarn-submit.sh \
     --app-name=random-datagen \
     --container-memory=4096 \
-    --kerberos-user=dev@DEV.FRISCH.COM \
+    --kerberos-user=dev@FRISCH.COM \
     --keytab=/home/dev/dev.keytab \
     --app-files=/home/root/random-datagen/config.properties,/home/root/random-datagen/log4j2.properties,/home/root/random-datagen/model.json,/home/root/random-datagen/random-datagen.jar \
     /home/root/yarn-submit/commandsToLaunch"
---
+++ src/main/resources/overallLaunch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export HOST=ccycloud-1.cannes.root.hwx.site
+
+src/main/resources/launchToPlatform.sh
